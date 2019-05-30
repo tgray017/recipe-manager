218bae6cb4e1f2f2574cb72c1bc530ea0e4feb88
Thomas Gray <tgray017@gmail.com>
2019-05-30 23:42:20 +0000

    Add migrations for users, recipes, and ingredients

---
+++ db/migrate/20190530233350_create_recipes_and_ingredients.rb
@@ -0,0 +1,16 @@
+class CreateRecipesAndIngredients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+    end
+    
+    create_table :ingredients do |t|
+      t.string :name
+    end
+    
+    create_table :recipes_ingredients, id: false do |t|
+      t.belongs_to :recipe, index: true
+      t.belongs_to :ingredient, index: true
+    end
+  end
+end
