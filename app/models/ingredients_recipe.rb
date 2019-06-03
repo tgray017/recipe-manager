b523a74f0636a4abba9855fb6ffba7c67d37eed8
Thomas Gray <tgray017@gmail.com>
2019-06-03 13:28:30 +0000

    Add find_quantity helper method

--- app/models/ingredients_recipe.rb
+++ app/models/ingredients_recipe.rb
@@ -4,8 +4,18 @@ class IngredientsRecipe < ActiveRecord::Base
   has_one :unit
 
 def self.find_quantity(recipe, ingredient)
+  query = <--SQL
+    select ir.quantity
+    from
+      ingredients_recipes ir
+      join ingredients i on ir.ingredient_id = i.id
+      join recipes r on ir.recipe_id = r.id
+    where
+      r.name = ?
+      and i.name = ?
+  SQL
   
-  
+  execute_sql(query, recipe.name, ingredient.name)
 end
 
 end
