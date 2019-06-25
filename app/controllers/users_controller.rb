14aefbbad80c5288cbeee90bd3ebeab65039bc53
Thomas Gray <tgray017@gmail.com>
2019-06-25 12:26:37 +0000

    Add authenticate method for user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
     erb :"users/login"
   end
   
+  post '/login' do
+    user = User.find_by("username" => params["username"])
+    if user.authenticate(params["password"])
+      redirect to '/recipes'
+    else
+      #set a flash message
+      redirect to '/login'
+    end
+  end
 end
