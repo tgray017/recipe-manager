33dbadeab6c56b83a7639ebf3eea5c4bedc4d2ad
Thomas Gray <tgray017@gmail.com>
2019-06-28 12:08:46 +0000

    Create login routes and add helper methods to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
   
+  
+  
   get '/login' do
     erb :"users/login"
   end
   
   post '/login' do
-    user = User.find_by("username" => params["username"])
-    if user.authenticate(params["password"])
+    user = User.find_by(:username => params[:username])
+    if !!user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect to '/recipes'
     else
       #set a flash message
       redirect to '/login'
     end
   end
+  
+  
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :recipes
   has_secure_password
+  
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+  
+  def self.logged_in?(session)
+    !!session[:user_id]
+  end  
+  
 end
+
