e8ba426b55597e7393fcb789cec734a4e811b79f
Thomas Gray <tgray017@gmail.com>
2019-06-28 12:25:29 +0000

    Add helper methods for user controller and redirect if logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,33 @@
 class UsersController < ApplicationController
   
+  get '/signup' do
+    if User.logged_in?(session)
+      redirect to '/recipes'
+    else
+      erb :"users/signup"
+    end
+  end
   
+  post '/signup' do
+    if invalid_signup?
+      #set a flash message - please fill out all fields
+      redirect to '/signup'
+    elsif !!User.find_by(:username => params[:username])
+      #set a flash message - user already exists or not all fields are filled out
+      redirect to '/signup'
+    else
+      user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      session[:user_id] = user.id
+      redirect to '/recipes'
+    end
+  end
   
   get '/login' do
-    erb :"users/login"
+    if User.logged_in?(session)
+      redirect to '/recipes'
+    else
+      erb :"users/login"
+    end
   end
   
   post '/login' do
@@ -12,12 +36,21 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect to '/recipes'
     else
-      #set a flash message
+      #set a flash message - wrong password
       redirect to '/login'
     end
   end
   
   
+  #### Helper methods ####
+  
+  def invalid_signup?
+    params[:email].empty? || params[:username].empty? || params[:password].empty?
+  end
+  
+  def invalid_login?
+    params[:username].empty? || params[:password].empty?
+  end
   
   
 end
