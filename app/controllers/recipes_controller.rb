75b416aa51989991f6cf5dcf24f97dab37e29ff2
Thomas Gray <tgray017@gmail.com>
2019-07-01 11:50:00 +0000

    Create recipes/:id route

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,25 @@
 class RecipesController < ApplicationController
   
   get '/recipes' do
-    erb :"recipes/recipes"
+    if User.logged_in?(session)
+      erb :"recipes/index"
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get '/recipes/:id' do
+    if User.logged_in?(session)
+      if !!Recipe.find(params[:id])
+        recipe = Recipe.find(params[:id])
+        erb :"recipes/show"
+      else
+        #set a flash message - recipe doesn't exist
+        redirect to '/recipes'
+      end
+    else
+      redirect to '/login'
+    end
   end
   
 end
