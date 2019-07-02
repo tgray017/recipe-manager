6e7428ab03fc664af5afccf76d7e3d31d530beb2
Thomas Gray <tgray017@gmail.com>
2019-07-02 13:05:42 +0000

    Add helper methods to determine valid recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,13 +27,30 @@ class RecipesController < ApplicationController
   end
   
   post '/recipes' do
-
-    recipe = Recipe.create(:name => params[:recipe][:name], :creator => User.current_user(session), :directions => params[:recipe][:directions], :total_prep_time => params[:recipe][:total_prep_time])
-    params[:recipe][:ingredients].each do |i|
-      binding.pry
-      ingredient = Ingredient.create(:name => i[:name], :quantity => i[:quantity], :unit => i[:unit])
+    if invalid_recipe?
+      #set flash message - make sure your recipe has a name, directions, total prep time, and at least one ingredient with a quantity
+      redirect to '/recipes/new'
+    else
+      recipe = Recipe.create(:name => params[:recipe][:name], :creator => User.current_user(session), :directions => params[:recipe][:directions], :total_prep_time => params[:recipe][:total_prep_time])
+      params[:recipe][:ingredients].each do |i|
+        recipe.ingredients.create(:name => i[:name], :quantity => i[:quantity], :unit => i[:unit]) unless empty_ingredient?(i)
+      end
     end
-    binding.pry
+  end
+  
+  
+  ##### Helper methods #####
+  
+  def invalid_recipe?
+    invalid_ingredients? || params[:recipe][:name].empty? ||  params[:recipe][:directions].empty? ||  params[:recipe][:total_prep_time].empty?
+  end
+  
+  def invalid_ingredients?
+    params[:recipe][:ingredients].any? {|i| !empty_ingredient?(i) && (i[:name].empty? || i[:quantity].empty?)} || params[:recipe][:ingredients].all? {|i| i[:name].empty?}
+  end
+  
+  def empty_ingredient?(ingredient)
+    ingredient.values.all? {|v| v.empty?}
   end
   
 
