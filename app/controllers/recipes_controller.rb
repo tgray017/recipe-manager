ac9ddf81b3ddc515d57cac1766bfeef32e4915ea
Thomas Gray <tgray017@gmail.com>
2019-07-08 12:19:35 +0000

    Clean up logic for user and recipe assignment to be consistent across controller actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,7 @@
 class RecipesController < ApplicationController
   
   get '/recipes' do
-    if User.logged_in?(session)
-      @user = User.current_user(session)
-    end
+    @user = User.current_user(session) if User.logged_in?(session)
     erb :"recipes/index"
   end
   
@@ -16,12 +14,10 @@ class RecipesController < ApplicationController
   end
   
   get '/recipes/:id' do
-    if User.logged_in?(session)
-      @user = User.current_user(session)
-    end
+    @recipe = Recipe.find_by_id(params[:id])
+    @user = User.current_user(session) if User.logged_in?(session)
     
-    if !!Recipe.find(params[:id])
-      @recipe = Recipe.find(params[:id])
+    if !!@recipe
       erb :"recipes/show"
     else
       #set a flash message - recipe doesn't exist
@@ -30,20 +26,25 @@ class RecipesController < ApplicationController
   end
   
   post '/recipes' do
-    if invalid_recipe?
-      #set flash message - make sure your recipe has a name, directions, total prep time, and at least one ingredient with a quantity
-      redirect to '/recipes/new'
-    else
-      recipe = Recipe.create(:name => params[:recipe][:name], :creator => User.current_user(session), :directions => params[:recipe][:directions], :total_prep_time => params[:recipe][:total_prep_time])
-      params[:recipe][:ingredients_attributes].each do |i|
-        recipe.ingredients.create(:name => i[:name], :quantity => i[:quantity], :unit => i[:unit]) unless empty_ingredient?(i)
+    if User.logged_in?(session)
+      if invalid_recipe?
+        #set flash message - make sure your recipe has a name, directions, total prep time, and at least one ingredient with a quantity
+        redirect to '/recipes/new'
+      else
+        recipe = Recipe.create(:name => params[:recipe][:name], :creator => User.current_user(session), :directions => params[:recipe][:directions], :total_prep_time => params[:recipe][:total_prep_time])
+        params[:recipe][:ingredients_attributes].each do |i|
+          recipe.ingredients.create(:name => i[:name], :quantity => i[:quantity], :unit => i[:unit]) unless empty_ingredient?(i)
+        end
+        redirect to "/recipes/#{recipe.id}"
       end
-      redirect to "/recipes/#{recipe.id}"
+    else
+      redirect to '/login'
     end
   end
   
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
+    
     if User.logged_in?(session)
       if !!@recipe && User.current_user(session).recipes.include?(@recipe)
         erb :"recipes/edit"
@@ -57,23 +58,39 @@ class RecipesController < ApplicationController
   end
     
   patch '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    if invalid_recipe?
-      #set flash message - make sure your recipe has a name, directions, total prep time, and at least one ingredient with a quantity
-      redirect to "/recipes/#{@recipe.id}/edit"
+    @recipe = Recipe.find_by_id(params[:id])
+    
+    if User.logged_in?(session)
+      if !!@recipe && User.current_user(session).recipes.include?(@recipe)
+        if invalid_recipe?
+          #set flash message - make sure your recipe has a name, directions, total prep time, and at least one ingredient with a quantity
+          redirect to "/recipes/#{@recipe.id}/edit"
+        else
+          @recipe.update(params[:recipe])
+          redirect to "/recipes/#{@recipe.id}"
+        end
+      else
+        #set flash message - recipe does not exist or does not belong to you
+        redirect to '/recipes'
+      end
     else
-      @recipe.update(params[:recipe])
-      redirect to "/recipes/#{@recipe.id}"
+      redirect to '/login'
     end
   end
   
   delete '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
-    if User.logged_in?(session) && User.current_user(session).recipes.include?(@recipe)
-      @recipe.destroy
-      redirect to "/recipes"
+    
+    if User.logged_in?(session)
+      if !!@recipe && User.current_user(session).recipes.include?(@recipe)
+        @recipe.destroy
+        redirect to "/recipes"
+      else
+        #set flash message - recipe does not exist or does not belong to you
+        redirect to "/recipes/#{@recipe.id}"
+      end
     else
-      redirect to "/recipes/#{@recipe.id}"
+      redirect to '/login'
     end
   end
   
